73b81da4da4a944a13532cd5fddb94360185d759
bot-rogerthat <himpocan@yandex.ru>
2020-10-28 18:26:01 +0300

    fix todos

--- rest-service/src/main/java/com/spring/boot/rest/service/api/rest/ArticleRestController.java
+++ rest-service/src/main/java/com/spring/boot/rest/service/api/rest/ArticleRestController.java
@@ -11,12 +11,10 @@ public class ArticleRestController {
 //    @GetMapping("{name}")
 //    public String getByNameAsync(@PathVariable String name) {
 //        Context<Object> context = new Context<>(name);
-//        //todo before log start process
 //        log.info("uuid: {}, system: {}, call: {}, data: {}", context.getUuid(), this.getClass().getSimpleName(), "start process", name);
 //        GetArticleRequest request = new GetArticleRequest();
 //        request.setName(name);
 //        soapGetArticleAsyncService.send(new Context<>(context, soapGetArticleAsyncService.getSystem(), request));
-//        //todo after log end process
 //        log.info("uuid: {}, system: {}, call: {}, data: {}", context.getUuid(), this.getClass().getSimpleName(), "end process", "ok");
 //        return "ok";
 //    }
